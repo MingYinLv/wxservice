34f2d2edf0a63fae3d6e04d253d318c71545c6a9
lvmingyin <lvmingyin@vip.qq.com>
2017-09-06 17:21:40 +0800

    parser xml

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const MongoSession = mongoStore(session);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text({ type: 'text/xml' }));
 
 routes(app);
 
