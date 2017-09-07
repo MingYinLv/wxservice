8884547b287cce8d7df73aa9a99317b7762c04aa
lvmingyin <lvmingyin@vip.qq.com>
2017-09-07 10:25:48 +0800

    array

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ const MongoSession = mongoStore(session);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(xmlparser());
+app.use(xmlparser({
+  explicitArray: true,
+}));
 
 routes(app);
 
--- routes/message.js
+++ routes/message.js
@@ -2,10 +2,39 @@
  * Created by MingYin Lv on 2017/9/7 上午9:59.
  */
 
+function convertXmlJson(body) {
+  const xml = body.xml;
+  const result = {};
+  Object.keys(xml).forEach((n) => {
+    result[n] = xml[n].join(',');
+  });
+  return result;
+}
+
+export const MessageType = {
+  TEXT: 'text',
+  EVENT: 'event',
+};
+
+export const EventType = {
+  SUBSCRIBE: 'subscribe',     // 订阅事件
+  UNSUBSCRIBE: 'unsubscribe', // 取消订阅事件
+};
+
+const eventHandler = {
+  [EventType.SUBSCRIBE]: ({ body }, req, res) => {
+
+  },
+};
 
 export default function (req, res) {
-  console.log(req.body);
-  res.end(`<xml>
+  const body = req.body;
+  console.log(body);
+  // const body = convertXmlJson(req.body);
+  if (body.msgtype === MessageType.event) {
+    typeof eventHandler[body.event] === 'function' && eventHandler[body.event]({ body }, req, res);
+  } else {
+    res.end(`<xml>
             <ToUserName><![CDATA[ovHJZ0VpazB47iXZossvjYLnBeVk]]></ToUserName>
             <FromUserName><![CDATA[gh_aea95a658979]]></FromUserName>
             <CreateTime>${Date.now()}</CreateTime>
@@ -13,4 +42,5 @@ export default function (req, res) {
             <Content><![CDATA[自动回复]]></Content>
             </xml>
   `);
+  }
 }
